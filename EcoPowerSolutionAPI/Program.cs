b72d23bbf1e3b077f7f8ea40096f4f1e7dcd44d1
Sibusiso Mtimkulu <mtimkulu18@gmail.com>
2023-08-28 17:43:40 +0200

    Project 2 API Update
    All controllers added

--- EcoPowerSolutionAPI/Program.cs
+++ EcoPowerSolutionAPI/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<EcoPowerSolutionsContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
