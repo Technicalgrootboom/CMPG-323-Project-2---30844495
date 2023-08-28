27ece1ad6c1ed41ee057fe5737fdc00f774942e4
Sibusiso Mtimkulu <mtimkulu18@gmail.com>
2023-08-28 13:49:59 +0200

    Project 2 Updated
    Project Restarted. About to Scaffold.

---
+++ EcoPowerSolutionAPI/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+//using EcoPowerSolutionAPI.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json","EcoPowerSolutions");
+        options.RoutePrefix = string.Empty;
+    }
+    );
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
