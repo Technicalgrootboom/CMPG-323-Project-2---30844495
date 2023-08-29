4e245febbdd1dc133e5de10131fec2e312e10930
Sibusiso Mtimkulu <mtimkulu18@gmail.com>
2023-08-29 21:21:19 +0200

    Project Update
    One Error. Still have to branch

---
+++ EcoPowerSolutionAPI/Controllers/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace EcoPowerSolutionAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecast : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+    private readonly ILogger<WeatherForecast> _logger;
+    }
+}
